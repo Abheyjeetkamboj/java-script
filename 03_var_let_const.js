ed72b90f06681e74f00c7be851de2148038b6c60
abheyjeet <abheyjeet100@gmail.com>
2025-08-11 17:29:45 +0530

    variables

---
+++ 03_var_let_const.js
@@ -0,0 +1,13 @@
+// // var a = 4;
+// const a = 4;
+// console.log(a);
+// a = 6;
+// console.log(a);
+
+// console.log("After error");
+
+
+var a = 5;
+console.log(a);
+var a = 7; //this will not give an error
+console.log(a);
