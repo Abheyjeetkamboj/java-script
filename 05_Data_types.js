b07488f2dc657eda5f8f1e543d138306e0be2775
abheyjeet <abheyjeet100@gmail.com>
2025-08-11 19:25:16 +0530

    data types

---
+++ 05_Data_types.js
@@ -0,0 +1,11 @@
+a = 4;
+b = 3.14;
+c = "abheyjeet";
+d = true;
+e = 'q';
+
+console.log(a,typeof(a));
+console.log(b,typeof(b));
+console.log(c,typeof(c));
+console.log(d,typeof(a));
+console.log(e,typeof(a)); 
