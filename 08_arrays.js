1dacebb5f3823efb9a1446f39be27d529161162f
abheyjeet <abheyjeet100@gmail.com>
2025-08-12 23:30:38 +0530

    arrays

---
+++ 08_arrays.js
@@ -0,0 +1,16 @@
+arr = [1,2,4,6,3];
+n = arr.length;
+console.log("Size of array is",n);
+console.log(arr);
+// console.log(arr[2]);
+// for(i = 0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+arr.push(9);
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift(2);
+console.log(arr);
+arr.shift();
+console.log(arr);
