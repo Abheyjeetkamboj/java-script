1275df82668129a97c2a8ab26e4422f5744ac342
abheyjeet <abheyjeet100@gmail.com>
2025-08-12 23:34:11 +0530

    for of loops

---
+++ 10_forofloop.js
@@ -0,0 +1,13 @@
+brr = [1,2,3,4];
+
+for(let i of brr){
+    i*=2;
+    console.log(i);
+}
+console.log(brr);
+
+// console.log(brr);
+// for(i = 0;i<7;i++){
+//     brr[i] *=2;
+
+// console.log(brr);
