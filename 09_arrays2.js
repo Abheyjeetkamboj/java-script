98aa6bcaae3cc2341227f5d0aed27fb3d783e20d
abheyjeet <abheyjeet100@gmail.com>
2025-08-12 23:32:32 +0530

    arrays

---
+++ 09_arrays2.js
@@ -0,0 +1,15 @@
+// const a = [4,3,6,5];
+// console.log(a);
+// a[0]= 9;
+// console.log(a);
+//a = 4;//this will not work
+arr = [2,true,"abheyjeet",[4,3,6,7],5.5];
+console.log(arr);
+
+
+
+
+//2d arrays
+
+arr2 = [[1,2,3],[15,7,8,2,5]];
+console.log(arr2);
