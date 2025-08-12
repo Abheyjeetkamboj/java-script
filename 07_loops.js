ce8a0e0332a3211b6b69d420db94b0dc117af6e7
abheyjeet <abheyjeet100@gmail.com>
2025-08-12 23:30:07 +0530

    loops

---
+++ 07_loops.js
@@ -0,0 +1,22 @@
+/*// for(i = 1;i<=10;i++){
+//     console.log(i);
+// }
+i = 10;
+while(i>=0){
+    console.log(i);
+    i--;
+}
+do{
+    console.log(i)
+    i++;
+}while(i<=10)
+
+    */
+
+   for(i = 1;i<=3;i++){
+    a = "";
+    for(j = i;j<=3;j++){
+        a+="*";
+    }
+    console.log(a);
+}
