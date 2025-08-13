0b40c18e196a1cebfaa03cbfd3be0455ca5f34c9
abheyjeet <abheyjeet100@gmail.com>
2025-08-14 00:27:09 +0530

    .\16_objects.js

---
+++ 16_objects.js
@@ -0,0 +1,15 @@
+let x = {
+    name: "abheyjeet",
+    age : 19,
+    'nick name':"Abhi",
+    percentage: 85,
+    isMarried: false,
+};
+console.log(x);
+console.log(x.age);
+console.log(x['name']);
+console.log(x['age']);
+x['age'] = 30;
+console.log(x['age']);
+x['age'] = 40;
+console.log(x.age);
