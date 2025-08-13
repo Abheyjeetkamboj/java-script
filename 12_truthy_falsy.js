bc220a79795db7b3d01f05f8fa3c51847a145a8d
abheyjeet <abheyjeet100@gmail.com>
2025-08-13 15:19:33 +0530

    truthy falsy conditions

---
+++ 12_truthy_falsy.js
@@ -0,0 +1,10 @@
+//falsy values => false condition,false,0,null,NaN,undefined
+//truthy values => everything expect falsy values
+if(true){
+    console.log("Hello");
+}
+else{
+    console.log("Mello");
+}
+
+console.log(typeof(a));//undefined because we havent define a any where
