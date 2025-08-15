fa70e5671a576daf9da16bd3dc68829aae543040
abheyjeet <abheyjeet100@gmail.com>
2025-08-15 21:37:14 +0530

    mathematicals functions

---
+++ 19_Mathematicals_functions.js
@@ -0,0 +1,8 @@
+console.log(Math.abs(7));//turn negative to positive
+console.log(Math.max(4,6,8,1,12,124,54,35));//max
+console.log(Math.min(4,6,8,1,12,124,54,35));//min
+console.log(Math.floor(7,66));//return floor means 7
+console.log(Math.ceil(7.66)); //return 8
+console.log(Math.pow(2,5));//2^5;
+console.log(Math.sqrt(4)); //2
+console.log(Math.cbrt(512)); //8
