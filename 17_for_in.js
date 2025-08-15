8c6261d565d779ab81699c9812c82a56c11c94ac
abheyjeet <abheyjeet100@gmail.com>
2025-08-15 21:36:23 +0530

    for in loop

---
+++ 17_for_in.js
@@ -0,0 +1,10 @@
+let x = {
+    name: "abheyjeet",
+    age : 19,
+    'nick name':"Abhi",
+    percentage: 85,
+    isMarried: false,
+};
+for (const key in x) {
+    console.log(key,":",x[key]);
+}
